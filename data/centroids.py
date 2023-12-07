fc3ece82f9c736ccf93d86ed937aebf28f6315cb
luismatos2001 <up201905962@fe.up.pt>
2023-12-07 15:57:49 +0000

    Changed field names to lat and lon

--- data/centroids.py
+++ data/centroids.py
@@ -1,11 +1,10 @@
 import csv
-from shapely.geometry import MultiPolygon
 from shapely.ops import cascaded_union
 from shapely.wkt import loads
 
 
 # Read the CSV file
-csv_file_path = 'ine_2021_bom_wgs.csv'  # Update with your CSV file path
+csv_file_path = './ine/ine_2021_bom_wgs.csv'  # Update with your CSV file path
 output_file_path = 'centroids.csv'
 
 # Assuming the column containing WKT geometries is named 'wkt_geom'
@@ -25,7 +24,7 @@ with open(csv_file_path, 'r', newline='', encoding='utf-8') as csvfile:
     # Create a new CSV file for writing the results
     with open(output_file_path, 'w', newline='', encoding='utf-8') as output_csvfile:
         # Define the fieldnames for the output CSV file
-        fieldnames = reader.fieldnames + ['center_x', 'center_y']
+        fieldnames = reader.fieldnames + ['lon', 'lat']
         
         # Create a CSV writer
         writer = csv.DictWriter(output_csvfile, fieldnames=fieldnames)
@@ -36,8 +35,8 @@ with open(csv_file_path, 'r', newline='', encoding='utf-8') as csvfile:
         for row in reader:
             # Extract the geometric center and add it to the row
             center_x, center_y = extract_geometric_center(row[wkt_geom_column_name])
-            row['center_x'] = center_x
-            row['center_y'] = center_y
+            row['lon'] = center_x
+            row['lat'] = center_y
             
             # Write the updated row to the output CSV file
             writer.writerow(row)
