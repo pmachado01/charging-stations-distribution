fa3c8abd87bb7d7c0da2cc39b697c8472687ac2c
Francisco Cerqueira <up201905337@fe.up.pt>
2023-12-14 15:26:10 +0000

    chore: restructure project folders

---
+++ src/utils/constants.py
@@ -0,0 +1,6 @@
+class Constants:
+    class Data:  # Data constants
+        RAW_DATA_PATH = "data/raw"
+        PROCESSED_DATA_PATH = "data/processed"
+        PROCESSED_CENTROIDS_FILE_NAME = "centroids.csv"
+        PROCESSED_STATIONS_FILE_NAME = "stations.csv"
